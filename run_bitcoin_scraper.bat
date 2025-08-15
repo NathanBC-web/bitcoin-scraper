c3388951c3954cc739894c1e0969db79cfdc7018
NathanBC-web <nathan.bc@outlook.com>
2025-08-15 13:57:26 +0200

    Updated .bat file to use virtual environment Python and set FRED_API_KEY

--- run_bitcoin_scraper.bat
+++ run_bitcoin_scraper.bat
@@ -1,4 +1,5 @@
 @echo off
+set FRED_API_KEY=2c224f339cc10721c3896180e9fdb66d
 cd "C:\Users\ArhanPeek\OneDrive - Universal IT B.V\Documents\Crypto"
-python bitcoin_scraper.py
+"C:\Users\ArhanPeek\OneDrive - Universal IT B.V\Documents\Crypto\.venv\Scripts\python.exe" bitcoin_scraper.py
 pause
